98ec2443868b9c0c7b926b7b4a23bf435a4dbd1f
RostSLO <63428412+RostSLO@users.noreply.github.com>
2021-04-09 18:15:54 -0600

    Few more katas from CodeWars

--- CodeWarsSolutions.py
+++ CodeWarsSolutions.py
@@ -1,14 +1,46 @@
 
-res = song_decoder("AWUBBWUBC")
-print(res)
 
 
-res = song_decoder("AWUBWUBWUBBWUBWUBWUBC")
-print(res)
+
+res = find_even_index([1,2,3,4,3,2,1])
+print(str(res))
 
 
-res = song_decoder("WUBAWUBBWUBCWUB")
-print(res)
+res = find_even_index([1,100,50,-51,1,1])
+print(str(res))
+
+
+res = find_even_index([1,2,3,4,5,6])
+print(str(res))
+
+
+
+
+
+'''
+==========================================================================
+Equal Sides Of An Array
+
+def find_even_index(arr):
+    
+    for i in range(0, len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    
+    return -1
+'''
+
+'''
+==========================================================================
+Create Phone Number
+
+def create_phone_number(n):
+    
+    return '({}{}{}) {}{}{}-{}{}{}{}'.format(*n)
+    
+    #return '({}) '.format(''.join(str(n[i]) for i in range(0, 3))) + '{}-'.format(''.join(str(n[i]) for i in range(3, 6))) + '{}'.format(''.join(str(n[i]) for i in range(6, 10)))
+    
+'''
 
 '''
 ==========================================================================
